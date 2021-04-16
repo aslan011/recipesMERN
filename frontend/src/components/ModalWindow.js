17b4f21efd7f426ca982d957c073a0f11d0d256f
aslan <social.smur@simplelogin.fr>
2021-04-16 08:32:40 +0100

    Edit array of items

--- frontend/src/components/ModalWindow.js
+++ frontend/src/components/ModalWindow.js
@@ -9,19 +9,9 @@ function ModalWindow (props) {
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
 
-  const splitToArray = (instructions, ingredients) => {
-    const splitInst = instructions.split(",");
-    const splitIngr = ingredients.split(",");
-    const updatedState = {...mealState};
-    updatedState['instructions'] = splitInst;
-    updatedState['ingredients'] = splitIngr;
-    setMealState(updatedState)
-  }
-
   const onSubmit = (e) => {
     setShow(false);
     e.preventDefault();
-    splitToArray(mealState.instructions, mealState.ingredients)
     const url = `http://localhost:9000/recipe/${mealState._id}`
     const request = new Request(url, {
       method: 'POST',
@@ -46,24 +36,38 @@ function ModalWindow (props) {
     setMealState(updatedState)
   };
 
+  const onChangeArray = (e) => {
+    const id = e.target.id;
+    const value = e.target.value;
+    const updatedState = {...mealState};
+    const array = updatedState[id];
+    const index = mealState[id].findIndex((a) => {return a == e.target.name})
+    array[index] = value;
+    setMealState(updatedState);
+  };
+
   const EditForm = () => {
     return (
-        <Form onChange={(e) => {onChange(e)}}>
-            <Form.Group controlId="name">
+        <Form>
+            <Form.Group controlId="name" onChange={(e) => {onChange(e)}}>
                 <Form.Label>Meal name</Form.Label>
                 <Form.Control type="text" defaultValue={`${mealState.name  || ""}`}/>
             </Form.Group>
-            <Form.Group controlId="cuisine">
+            <Form.Group controlId="cuisine" onChange={(e) => {onChange(e)}}>
                 <Form.Label>Cuisine</Form.Label>
                 <Form.Control type="text" defaultValue={`${mealState.cuisine  || ""}`}/>
             </Form.Group>
-            <Form.Group controlId="ingredients">
+            <Form.Group controlId="ingredients" onChange={(e) => {onChangeArray(e)}}>
                 <Form.Label>Ingredients</Form.Label>
-                <Form.Control as="textarea" rows={5} defaultValue={`${mealState.ingredients  || ""}`}/>
+                {mealState.ingredients.map(ingredient => {
+                  return <Form.Control type="text" name={ingredient} defaultValue={ingredient}/>
+                })} 
             </Form.Group>
-            <Form.Group controlId="instructions">
+            <Form.Group controlId="instructions" onChange={(e) => {onChangeArray(e)}}>
                 <Form.Label>Instructions</Form.Label>
-                <Form.Control as="textarea" rows={5} defaultValue={`${mealState.instructions || ""}`}/>
+                {mealState.instructions.map(instruction => {
+                  return <Form.Control type="text" name={instruction} defaultValue={instruction}/>
+                })} 
             </Form.Group>
         </Form>
     )
